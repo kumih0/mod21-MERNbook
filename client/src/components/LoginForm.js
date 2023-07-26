371057376439b13e13c0884cb72e5837b911821b
KT <132851569+kumih0@users.noreply.github.com>
2023-07-26 16:07:01 -0500

    importing apollo hooks and mutations

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,12 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+//import apollo hooks
+import { useMutation } from '@apollo/client';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+//importing mutation
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
@@ -25,20 +28,20 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
-    try {
-      const response = await loginUser(userFormData);
+    // try {
+    //   const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
+    //   const { token, user } = await response.json();
+    //   console.log(user);
+    //   Auth.login(token);
+    // } catch (err) {
+    //   console.error(err);
+    //   setShowAlert(true);
+    // }
 
     setUserFormData({
       username: '',
