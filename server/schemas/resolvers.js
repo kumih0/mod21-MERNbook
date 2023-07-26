722f23e6747b49d67a9aba1c93931688548dd4d1
KT <132851569+kumih0@users.noreply.github.com>
2023-07-25 23:33:08 -0500

    finished add user and save book

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
+const { saveBook } = require('../controllers/user-controller');
 
 const resolvers = {
     Query: {
@@ -33,5 +34,21 @@ const resolvers = {
 
             const token = signToken(user);
             return {token, user};
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return {token, user};
         }
-        
+        saveBook: async (parent, {input}, context) => {
+            if(context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$push: {savedBooks: input}},
+                    {new: true}
+                );
+
+                return updatedUser;
+            }
+            
