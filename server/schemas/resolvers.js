833b5407101712f05c47f7535d87dc2879b69d9c
KT <132851569+kumih0@users.noreply.github.com>
2023-07-25 21:17:49 -0500

    finished query for resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,3 +2,19 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({_id: context.user._id})
+                    .select('-__v -password')
+                    .populate('books')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+
+    
