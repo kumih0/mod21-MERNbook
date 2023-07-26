250663c85d317f10c40523598fd8604f30c57dd5
KT <132851569+kumih0@users.noreply.github.com>
2023-07-25 21:24:44 -0500

    adding mutations, login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,4 +17,21 @@ const resolvers = {
         }
     },
 
-    
+    Mutation: {
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return {token, user};
+        }
+        
