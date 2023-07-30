e3cc780f04c9308657914e6b4c20417e219532e2
KT <132851569+kumih0@users.noreply.github.com>
2023-07-30 18:24:43 -0500

    fixing mistakes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,12 +16,6 @@ const resolvers = {
 
             throw new AuthenticationError('Not logged in');
         },
-        // get all users
-        users: async () => {
-            return User.find()
-                .select('-__v -password')
-                .populate('books')
-        },
     },
     Mutation: {
         login: async (parent, { email, password }) => {
